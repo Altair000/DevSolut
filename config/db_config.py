5792954344a28b531012a8e092a96b5239d6714b
Altair000 <raydiel_e@icloud.com>
2024-11-27 09:29:53 -0500

    Delete config directory

--- config/db_config.py
+++
@@ -1,8 +0,0 @@
-# DEPENDENCIAS ###################################################################
-import os
-
-# VARIABLES ######################################################################
-host = os.getenv('HOST', 'ep-plain-smoke-a5rwbc7s.us-east-2.aws.neon.tech')
-user = os.getenv('USER', 'chkdb_owner')
-password= os.getenv('PASS', 'DLMPk7Irq6gE')
-dbname = os.getenv('DB', 'chkdb')
